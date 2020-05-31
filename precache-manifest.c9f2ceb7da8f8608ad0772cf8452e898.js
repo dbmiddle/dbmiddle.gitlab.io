0683091e650f1922d8aece92e85fbf232f0368c9
dbmiddle <dbmiddle39@gmail.com>
2020-05-31 18:23:10 -0400

    Updates

---
+++ precache-manifest.c9f2ceb7da8f8608ad0772cf8452e898.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b074ae996b3725209cc9df5891c460d9",
+    "url": "/devon-middleton/index.html"
+  },
+  {
+    "revision": "9f5947b3acfd9c55db22",
+    "url": "/devon-middleton/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "b7bf3a5e48c1410261b4",
+    "url": "/devon-middleton/static/css/main.c7722c0f.chunk.css"
+  },
+  {
+    "revision": "9f5947b3acfd9c55db22",
+    "url": "/devon-middleton/static/js/2.2f13d2e7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/devon-middleton/static/js/2.2f13d2e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7bf3a5e48c1410261b4",
+    "url": "/devon-middleton/static/js/main.ee6412bb.chunk.js"
+  },
+  {
+    "revision": "49189d3d6cc8909d67a3",
+    "url": "/devon-middleton/static/js/runtime-main.eeb55139.js"
+  },
+  {
+    "revision": "ef0695b7b51f39598f1d068f5f7a7716",
+    "url": "/devon-middleton/static/media/BugTracker.ef0695b7.jpg"
+  },
+  {
+    "revision": "1d9e16b6c5e509299069428e1efbc65f",
+    "url": "/devon-middleton/static/media/ConnectFour.1d9e16b6.jpg"
+  },
+  {
+    "revision": "f32278b6d3fd77bbfc4cce679b123262",
+    "url": "/devon-middleton/static/media/Devon.f32278b6.jpg"
+  },
+  {
+    "revision": "819b9776f155e5a99b5dbfa1a7aa0d99",
+    "url": "/devon-middleton/static/media/RockPaperScissors.819b9776.jpg"
+  },
+  {
+    "revision": "bb65386e255e3717cf8ca84224d47edb",
+    "url": "/devon-middleton/static/media/Sokoban.bb65386e.jpg"
+  },
+  {
+    "revision": "de891873de536e9dc5438d88778f8bc9",
+    "url": "/devon-middleton/static/media/TowerOfHanoi.de891873.jpg"
+  },
+  {
+    "revision": "62cc47fc9f540e2be5868a26c94106ab",
+    "url": "/devon-middleton/static/media/TwitterClone.62cc47fc.jpg"
+  },
+  {
+    "revision": "1e62e0f83298d3c52c0385c3713054ad",
+    "url": "/devon-middleton/static/media/githublogo.1e62e0f8.jpg"
+  }
+]);
